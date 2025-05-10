2978471b1719efd2929c789a8e5604b36a147fe7
Valeron01 <electronpochta3011@mail.ru>
2025-05-10 22:59:04 +0300

    Added support of wcdi

--- Core/Src/usb_descriptors.c
+++ Core/Src/usb_descriptors.c
@@ -8,31 +8,54 @@
 
 #include "tusb.h"
 
-// USB Device Descriptor
-tusb_desc_device_t const desc_device =
-{
+// Microsoft OS Descriptor (WCID 1.0)
+#define WCID_DESCRIPTOR_SIZE (sizeof(wcid_descriptor))
+
+// Microsoft OS 1.0 Descriptor
+uint8_t const wcid_descriptor[] = {
+    0x12,           // Descriptor size (18 bytes)
+    0x03,           // Descriptor type: Microsoft OS Descriptor (0x03)
+    0x00, 0x01,     // Version 1.0
+    0x00, 0x00,     // Reserved
+    0x08,           // Total length
+    0x00, 0x01,     // Vendor ID (custom)
+    0x00, 0x01,     // Product ID (custom)
+    0x00,           // Flags
+    0x01,           // Number of descriptors
+    0x01, 0x00,     // WCID descriptor type
+    0x00, 0x01,     // Vendor specific
+    0x02, 0x00      // Index of descriptors (always 1)
+};
+
+
+tusb_desc_device_t const desc_device = {
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,
+    .bcdUSB             = 0x0200,    // USB 2.0
     .bDeviceClass       = TUSB_CLASS_VENDOR_SPECIFIC,
     .bDeviceSubClass    = 0x00,
     .bDeviceProtocol    = 0x00,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-
-    .idVendor           = 0xCafe,
-    .idProduct          = 0x4001,
-    .bcdDevice          = 0x0100,
-
-    .iManufacturer      = 0x01,
-    .iProduct           = 0x02,
-    .iSerialNumber      = 0x03,
-
+    .idVendor           = 0x1234,    // Vendor ID
+    .idProduct          = 0x5678,    // Product ID
+    .bcdDevice          = 0x0100,    // Device version
+    .iManufacturer      = 0x01,      // String index 1
+    .iProduct           = 0x02,      // String index 2
+    .iSerialNumber      = 0x03,      // String index 3
     .bNumConfigurations = 0x01
 };
 
-uint8_t const * tud_descriptor_device_cb(void)
-{
-    return (uint8_t const *) &desc_device;
+uint8_t const * tud_descriptor_get_cb(uint8_t type, uint8_t index) {
+    // Handle the WCID descriptor request (GET_DESCRIPTOR 0xEE)
+    if (type == TUSB_DESC_DEVICE) {
+        return (uint8_t const *) &desc_device;
+    }
+
+    if (type == 0xEE) {  // WCID Microsoft OS Descriptor
+        return wcid_descriptor;
+    }
+
+    return NULL;
 }
 
 
